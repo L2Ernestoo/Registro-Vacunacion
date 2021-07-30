6f0a87d82b85567e9ee21be8093e9d9721f3f529
Ernestoo <ernestoorellana5267@gmail.com>
2021-07-29 21:18:20 -0600

    Proyecto Base

---
+++ src/main/java/com/example/registrovacunacion/RegistroVacunacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.registrovacunacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroVacunacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistroVacunacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/registrovacunacion/RegistroVacunacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registrovacunacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroVacunacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
